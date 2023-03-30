7ae054c56c24c0bd1a89c29e2695bc28f7efa774
drh <jmnabangi@gmail.com>
2023-03-30 21:20:38 +0300

    test gce log formatter
    Signed-off-by: drh <jmnabangi@gmail.com>

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,29 +3,38 @@ package logger
 import (
 	"USSD.sidooh/utils"
 	"fmt"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"time"
 )
 
-var UssdLog = &log.Logger{
+var UssdLog = &logrus.Logger{
 	Out: nil,
 }
 
-var ServiceLog = &log.Logger{
+var ServiceLog = &logrus.Logger{
 	Out: nil,
 }
 
 func Init() {
 	fmt.Println("Initializing USSD subsystem loggers")
 
-	UssdLog = log.New()
-	ServiceLog = log.New()
+	UssdLog = logrus.New()
+	ServiceLog = logrus.New()
 
 	env := viper.GetString("APP_ENV")
 
+	logger := viper.GetString("LOGGER")
+
+	fmt.Println(logger)
+
 	if env != "TEST" {
-		UssdLog.SetOutput(utils.GetLogFile("ussd-" + time.Now().Format("2006-01-02") + ".log"))
-		ServiceLog.SetOutput(utils.GetLogFile("service-" + time.Now().Format("2006-01-02") + ".log"))
+		if logger == "GCP" {
+			UssdLog.SetFormatter(NewGCEFormatter(true))
+			ServiceLog.SetFormatter(NewGCEFormatter(true))
+		} else {
+			UssdLog.SetOutput(utils.GetLogFile("ussd-" + time.Now().Format("2006-01-02") + ".log"))
+			ServiceLog.SetOutput(utils.GetLogFile("service-" + time.Now().Format("2006-01-02") + ".log"))
+		}
 	}
 }
