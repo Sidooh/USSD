641b0318a3f70e205624a9088c2f0f628bb3348e
DrH <jmnabangi@gmail.com>
2024-03-23 21:01:46 +0300

    Adds CORS middleware
    Signed-off-by: DrH <jmnabangi@gmail.com>

--- api/server.go
+++ api/server.go
@@ -16,7 +16,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Origin", "*") // Adjust origin as needed
 			//w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			//w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 			w.WriteHeader(http.StatusOK)
 			return
 		}
